691edaff001d2590abdff2a9b6ca07d1509d09dc
Tyler Jensen <tyler@theeventhelper.com>
2023-04-13 16:15:29 -0700

    Added server code and app code template

--- controller/server.ts
+++ controller/server.ts
@@ -1,40 +1,124 @@
-import * as WebSocket from 'ws';
-import * as http from 'http';
+import { Gpio } from 'onoff';
+import WebSocket, { Server } from 'ws';
 
-interface SensorData {
-    temperature: number;
-}
+const wss = new Server({ port: 8080 });
 
-function getSensorData(): SensorData {
-    return {
-        temperature: 0
+// Rotary Encoder
+const clk = new Gpio(23, 'in', 'both');
+const dt = new Gpio(24, 'in', 'both');
+const sw = new Gpio(12, 'in', 'rising', { debounceTimeout: 10 });
+
+// Ultrasonic Sensor
+const trig = new Gpio(5, 'out');
+const echo = new Gpio(6, 'in', 'both');
+
+// Optocoupler
+const optoOut = new Gpio(25, 'in', 'falling');
+
+// Stepper motor
+const dir = new Gpio(17, 'out');
+const pul = new Gpio(27, 'out');
+const en = new Gpio(22, 'out');
+
+let counter = 0;
+
+// Stepper motor rotation parameters
+let stepDelay = 1000; // Default delay between steps (microseconds)
+let direction: 0 | 1 = 1; // Default direction: 1 for clockwise, 0 for counterclockwise
+
+// Stepper motor rotation loop
+async function rotateStepper() {
+    // noinspection InfiniteLoopJS
+    while (true) {
+        dir.writeSync(direction);
+        en.writeSync(0);
+
+        pul.writeSync(1);
+        await sleep(1);
+        pul.writeSync(0);
+
+        await sleep(stepDelay);
     }
 }
 
-export const PORT = 8080;
+wss.on('connection', (ws: WebSocket) => {
+    console.log('Client connected');
 
-export function startWebSocketServer(): http.Server {
-    const server = http.createServer();
-    const wss = new WebSocket.Server({ server });
+    // Rotary Encoder
+    clk.watch((err, value) => {
+        if (err) throw err;
 
-    wss.on('connection', (ws: WebSocket) => {
-        console.log('Client connected');
+        const dtValue = dt.readSync();
+        if (value !== dtValue) {
+            counter++;
+        } else {
+            counter--;
+        }
 
-        // Listen for messages from the client
-        ws.on('message', (message: WebSocket.Data) => {
-            console.log(`Received message: ${message}`);
+        ws.send(JSON.stringify({ type: 'encoder', counter: counter }));
+    });
+
+    sw.watch((err) => {
+        if (err) throw err;
+        ws.send(JSON.stringify({ type: 'encoderButton', pressed: true }));
+    });
+
+    // Optocoupler
+    optoOut.watch((err) => {
+        if (err) throw err;
+        ws.send(JSON.stringify({ type: 'optocoupler', triggered: true }));
+    });
+
+    ws.on('message', async (message: WebSocket.Data) => {
+        const data = JSON.parse(message.toString());
+
+        // Update stepper motor direction
+        if (data.type === 'updateStepperDirection') {
+            direction = data.direction as 0 | 1;
+        }
+
+        // Update stepper motor step delay (speed)
+        if (data.type === 'updateStepperSpeed') {
+            stepDelay = data.stepDelay;
+        }
+
+        // Ultrasonic sensor
+        if (data.type === 'measureDistance') {
+            const distance = await measureDistance();
+            ws.send(JSON.stringify({ type: 'distance', distance: distance }));
+        }
+    });
+});
+
+function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function measureDistance(): Promise<number> {
+    return new Promise((resolve, reject) => {
+        let start: bigint;
+        let end: bigint;
+
+        echo.watch((err, value) => {
+            if (err) reject(err);
+
+            if (value === 1) {
+                start = process.hrtime.bigint();
+            } else {
+                end = process.hrtime.bigint();
+                echo.unwatchAll();
+                resolve(Number(end - start) / 1e6 / 2 / 29.1);
+            }
         });
 
-        // Send data to the client
-        setInterval(() => {
-            const sensorData = getSensorData();
-            ws.send(JSON.stringify(sensorData));
-        }, 1000); // Adjust the interval as needed
+        trig.writeSync(1);
+        setTimeout(() => {
+            trig.writeSync(0);
+        }, 10);
     });
-
-    server.listen(PORT, () => {
-        console.log(`WebSocket server started on port ${PORT}`);
-    });
-
-    return server;
 }
+
+// Start the stepper motor rotation loop
+// noinspection JSIgnoredPromiseFromCall
+rotateStepper();
+
---
+++ ui/src/App.tsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from 'react';
+
+interface EventData {
+  type: string;
+  [key: string]: any;
+}
+
+function App() {
+  const [ws, setWs] = useState<WebSocket | null>(null);
+  const [events, setEvents] = useState<EventData[]>([]);
+  const [direction, setDirection] = useState<number>(1);
+  const [stepDelay, setStepDelay] = useState<number>(1000);
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://' + window.location.hostname + ':8080');
+    setWs(ws);
+
+    ws.onopen = () => {
+      console.log('Connected to WebSocket server');
+    };
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data) as EventData;
+      setEvents((prevEvents) => {
+        const newEvents = [...prevEvents, data];
+        if (newEvents.length > 20) {
+          newEvents.shift();
+        }
+        return newEvents;
+      });
+    };
+
+    ws.onclose = () => {
+      console.log('Disconnected from WebSocket server');
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  const updateStepperDirection = () => {
+    if (ws) {
+      ws.send(
+          JSON.stringify({ type: 'updateStepperDirection', direction: direction })
+      );
+    }
+  };
+
+  const updateStepperSpeed = () => {
+    if (ws) {
+      ws.send(
+          JSON.stringify({ type: 'updateStepperSpeed', stepDelay: stepDelay })
+      );
+    }
+  };
+
+  return (
+      <div>
+        <h1>Raspberry Pi WebSocket Interface</h1>
+        <div>
+          <label>
+            Direction:{' '}
+            <select
+                value={direction}
+                onChange={(e) => setDirection(Number(e.target.value))}
+            >
+              <option value={1}>Clockwise</option>
+              <option value={0}>Counterclockwise</option>
+            </select>
+          </label>
+          <button onClick={updateStepperDirection}>Update Direction</button>
+        </div>
+        <div>
+          <label>
+            Step Delay:{' '}
+            <input
+                type="number"
+                value={stepDelay}
+                onChange={(e) => setStepDelay(Number(e.target.value))}
+            />
+          </label>
+          <button onClick={updateStepperSpeed}>Update Speed</button>
+        </div>
+        <h2>Events</h2>
+        <ul>
+          {events.map((event, index) => (
+              <li key={index}>{JSON.stringify(event)}</li>
+          ))}
+        </ul>
+      </div>
+  );
+}
+
+export default App;
