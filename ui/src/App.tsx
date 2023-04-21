5be9c4e135c26b5593d571d7ed7d824a56594a1f
Tyler Jensen <tyler@theeventhelper.com>
2023-04-21 10:49:22 -0700

    Added App.ts file

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,150 +1,109 @@
-import { Gpio } from 'onoff';
-import WebSocket, { Server } from 'ws';
+import { useEffect, useState } from 'react';
+import confetti from 'canvas-confetti';
+import './App.css';
 
-const wss = new Server({ port: 8080 });
-
-// Rotary Encoder
-const clk = new Gpio(23, 'in', 'both');
-const dt = new Gpio(24, 'in', 'both');
-const sw = new Gpio(12, 'in', 'rising', { debounceTimeout: 10 });
-
-// Optocouplers
-const optoOutPre = new Gpio(25, 'in', 'falling');
-const optoOutPost = new Gpio(6, 'in', 'falling');
-
-// Stepper motor
-const DIR_PIN = 20;
-const PUL_PIN = 21;
-const ENA_PIN = 16;
-const dir = new Gpio(DIR_PIN, 'out');
-const pul = new Gpio(PUL_PIN, 'out');
-const en = new Gpio(ENA_PIN, 'out');
-
-let counter = 0;
-
-// Stepper motor rotation parameters
-const stepDelay = .45; // Default delay between steps (microseconds)
-const direction: 0 | 1 = 1; // Default direction: 1 for clockwise, 0 for counterclockwise
-
-// Debounce configuration
-const DEBOUNCE_TIME = 50; // Debounce time in milliseconds
-let buttonPressTimestamp: number | null = null;
-
-let firstSensorTime: Date = new Date();
-let secondSensorTime: Date = new Date();
-let score = 0;
-let updateInProgress = false;
-
-const clients = new Set<WebSocket>();
-
-// Stepper motor rotation loop
-async function rotateStepper() {
-    en.writeSync(0);
-    // noinspection InfiniteLoopJS
-    while (true) {
-        dir.writeSync(direction);
-
-        pul.writeSync(1);
-        await sleep(stepDelay);
-        pul.writeSync(0);
-        await sleep(stepDelay);
-    }
+interface EventData {
+    type: string;
+    [key: string]: any;
 }
 
-function updateScore() {
-    const timeDiff: number = Math.abs(secondSensorTime.valueOf() - firstSensorTime.valueOf());
-    if(timeDiff < 1000) {
-        score++;
-        console.log("New Score", score);
-    } else {
-        score = 0;
-        console.log("Score Cleared", score);
-    }
-}
+function App() {
+    const [score, setScore] = useState(0);
+    const [isUpdated, setIsUpdated] = useState(false);
 
-clk.watch((err, value) => {
-    if (err) throw err;
-
-    const dtValue = dt.readSync();
-    if (value !== dtValue) {
-        counter++;
-    } else {
-        counter--;
-    }
-
-    // Send update to all connected clients
-    clients.forEach((client) => {
-        client.send(JSON.stringify({ type: 'encoder', counter: counter }));
-    });
-});
-
-// Handle button press event for rotary encoder
-sw.watch((err, value) => {
-    if (err) throw err;
-
-    if (value === 0) { // Button press detected
-        const currentTime = Date.now();
-        if (!buttonPressTimestamp || currentTime - buttonPressTimestamp > DEBOUNCE_TIME) {
-            buttonPressTimestamp = currentTime;
-
-            // Handle button press event
-            console.log('Button pressed');
-            clients.forEach((client) => {
-                client.send(JSON.stringify({ type: 'button', state: 'pressed' }));
-            });
+    useEffect(() => {
+        if (isUpdated) {
+            const timeoutId = setTimeout(() => setIsUpdated(false), 1000);
+            return () => clearTimeout(timeoutId);
         }
-    }
-});
+    }, [isUpdated]);
 
-// Pre-Wheel Sensor
-optoOutPre.watch((err) => {
-    if (err) throw err;
-    firstSensorTime = new Date();
+    const handleScoreChange = (score: number) => {
+        if(score === 0) {
+            window.location.reload();
+            return;
+        }
 
-    clients.forEach((client) => {
-        client.send(JSON.stringify({ type: 'pre-wheel', triggered: true }));
-    });
+        setScore(score);
+        setIsUpdated(true);
 
-    if (!updateInProgress) {
-        updateInProgress = true;
-        setTimeout(function () {
-            updateScore();
-            clients.forEach((client) => {
-                client.send(JSON.stringify({ type: 'score', score }));
+        const duration = Math.min((score * 1000)/2, 3000);
+        const end = Date.now() + duration;
+        const particleCount = Math.min(score*2, 20);
+
+        (function frame() {
+            // launch a few confetti from the left edge
+            confetti({
+                angle: 45,
+                spread: 55,
+                origin: { x: 0, y:1 },
+                gravity: .75,
+
+                startVelocity: 100,
+                particleCount: particleCount,
+                colors: [ '#ff3860', '#e85a74', '#960e28'],
             });
-            updateInProgress = false;
-        }, 1000);
-    }
-});
 
-// Post-Wheel Sensor
-optoOutPost.watch((err) => {
-    if (err) throw err;
-    secondSensorTime = new Date();
+            // and launch a few from the right edge
+            confetti({
+                angle: 135,
+                spread: 55,
+                origin: { x: 1, y:1 },
+                gravity: .75,
 
-    clients.forEach((client) => {
-        client.send(JSON.stringify({ type: 'post-wheel', triggered: true }));
-    });
-});
+                startVelocity: 100,
+                particleCount: particleCount,
+                colors: [ '#ff3860', '#e85a74', '#960e28'],
+            });
 
-wss.on('connection', (ws: WebSocket) => {
-    console.log('Client connected');
-    ws.send(JSON.stringify({ type: 'score', score }));
-    
-    clients.add(ws);
+            // keep going until we are out of time
+            if (Date.now() < end) {
+                requestAnimationFrame(frame);
+            }
+        }());
+    };
 
-    // ... (Other code remains the same)
+    useEffect(() => {
+        const ws = new WebSocket('ws://localhost:8080');
 
-    ws.on('close', () => {
-        clients.delete(ws);
-    });
-});
+        ws.onopen = () => {
+            console.log('Connected to WebSocket server');
+        };
 
-function sleep(ms: number): Promise<void> {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+        ws.onmessage = (event) => {
+            const data = JSON.parse(event.data) as EventData;
+            console.log("new event", data);
+            if(data.type === "score") {
+                handleScoreChange(data.score);
+            }
+        };
+
+        ws.onclose = () => {
+            console.log('Disconnected from WebSocket server');
+        };
+
+        return () => {
+            ws.close();
+        };
+    }, []);
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                height: '100vh',
+                backgroundImage: 'url(TaxEvader.jpg)',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }} onClick={function() {
+            const newScore = score + 1;
+            handleScoreChange(newScore);
+        }}>
+            <div className={`number ${isUpdated ? 'pop' : ''}`}>{score}</div>
+        </div>
+    );
 }
 
-// Start the stepper motor rotation loop
-// noinspection JSIgnoredPromiseFromCall
-rotateStepper();
-
+export default App;
